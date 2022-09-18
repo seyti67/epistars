4d0c5957ee42db8f51c8f8d3bcc4680cc7873749
seyti67 <74255469+seyti67@users.noreply.github.com>
2022-09-18 10:19:57 +0200

    bug fix: smoke transition

--- docs/_app/immutable/chunks/1-2c31684b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-ec27c6a8.js";export{t as component};
--- src/lib/scripts/animations/clip.ts
+++ src/lib/scripts/animations/clip.ts
@@ -13,12 +13,12 @@ export function clipPath(node: HTMLElement, { duration = 1000, delay = 0 }) {
 			setTimeout(() => {
 				for (let i = 0; i < 6; i++) {
 					if (fromTop) fluids.splat(0.5, eased, 0, -10);
-					else fluids.splat(eased, 0.5, -10, 0);
+					else fluids.splat(1 - eased, 0.5, 10, 0);
 				}
 				for (let i = 0; i < 20; i++) {
 					const y = Math.random();
 					if (fromTop) fluids.splat(y, eased, 0, -100);
-					else fluids.splat(eased, y, -100, 0);
+					else fluids.splat(1 - eased, y, 100, 0);
 				}
 			}, t * duration);
 			if (fromTop) {
@@ -26,7 +26,9 @@ export function clipPath(node: HTMLElement, { duration = 1000, delay = 0 }) {
 					100 - eased * 100
 				}%);`;
 			} else {
-				return `clip-path: polygon(0 0, ${eased * 100}% 0, ${eased * 100}% 100%, 0 100%);`;
+				return `clip-path: polygon(0 0, ${100 - eased * 100}% 0, ${
+					100 - eased * 100
+				}% 100%, 0 100%);`;
 			}
 		}
 	};
